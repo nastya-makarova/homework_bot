9c0553ef841361b753bc19abb535eb9b5c748413
Nastya Makarova <nastasia.konovalova@yandex.ru>
2024-09-02 11:45:07 +0400

    Installed python-dotenv; made env file; wrote check_tokens and get_api_answer func.

--- homework.py
+++ homework.py
@@ -1,11 +1,14 @@
-...
+import os
+import requests
+
+from dotenv import load_dotenv
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -20,7 +23,9 @@ HOMEWORK_VERDICTS = {
 
 
 def check_tokens():
-    ...
+    """Функция проверяет доступность переменных окружения."""
+    if PRACTICUM_TOKEN or TELEGRAM_TOKEN or TELEGRAM_CHAT_ID is None:
+        return False
 
 
 def send_message(bot, message):
@@ -28,7 +33,10 @@ def send_message(bot, message):
 
 
 def get_api_answer(timestamp):
-    ...
+    """Функция делает запрос к единственному эндпоинту API-сервиса."""
+    payload = {'from_date': timestamp}
+    homework_statuses = requests.get(ENDPOINT, headers=HEADERS, params=payload)
+    return homework_statuses.json()
 
 
 def check_response(response):
