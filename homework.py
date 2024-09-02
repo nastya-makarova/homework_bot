30c80735793741f2d964dff0b4b53da23d9b2763
Nastya Makarova <nastasia.konovalova@yandex.ru>
2024-09-02 23:21:53 +0400

    Added exceptions and all functions.

--- homework.py
+++ homework.py
@@ -1,7 +1,11 @@
 import os
 import requests
+import time
 
 from dotenv import load_dotenv
+from telebot import TeleBot
+
+from exceptions import TokenNotFoundError, TimestampError
 
 load_dotenv()
 
@@ -10,7 +14,7 @@ PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-RETRY_PERIOD = 600
+RETRY_PERIOD = 10
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
@@ -26,49 +30,74 @@ def check_tokens():
     """Функция проверяет доступность переменных окружения."""
     if PRACTICUM_TOKEN or TELEGRAM_TOKEN or TELEGRAM_CHAT_ID is None:
         return False
+    else:
+        return True
 
 
 def send_message(bot, message):
-    ...
+    """Функция отправляет сообщение в чат, определяемый TELEGRAM_CHAT_ID."""
+    # message = parse_status(homework)
+    bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
 
 
 def get_api_answer(timestamp):
     """Функция делает запрос к единственному эндпоинту API-сервиса."""
     payload = {'from_date': timestamp}
-    homework_statuses = requests.get(ENDPOINT, headers=HEADERS, params=payload)
-    return homework_statuses.json()
+    try:
+        homework_statuses = requests.get(
+            ENDPOINT,
+            headers=HEADERS,
+            params=payload
+        )
+    except Exception as error:
+        print(f'Ошибка при запросе к основному API: {error}')
+    else:
+        return homework_statuses.json()
 
 
 def check_response(response):
-    ...
+    """Функция проверяет ответ API на соответствие документации."""
+    # response = get_api_answer(timestamp)
+    return 'homeworks' in response and 'current_date' in response
 
 
 def parse_status(homework):
-    ...
-
+    """Функция извлекает статус конкретной домашней работы."""
+    # homework = homework_statuses.json().get('homeworks')[0]
+    # homework_statuses.json() = get_api_answer(timestamp)
+    homework_name = homework.get('homework_name')
+    status = homework.get('status')
+    verdict = HOMEWORK_VERDICTS.get(status)
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def main():
     """Основная логика работы бота."""
+    bot = TeleBot(token=TELEGRAM_TOKEN)
+    # timestamp = int(time.time())
+    timestamp = 0
 
-    ...
-
-    # Создаем объект класса бота
-    bot = ...
-    timestamp = int(time.time())
-
-    ...
+    if not isinstance(timestamp, int) or timestamp < 0:
+        raise TimestampError
 
     while True:
         try:
-
-            ...
-
+            response = get_api_answer(timestamp)
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            ...
-        ...
+        else:
+            print(response)
+            print('homeworks' in response and 'current_date' in response)
+            if not check_tokens(response):
+                raise TokenNotFoundError
+
+            homework = response.get('homeworks')[0]
+            print(homework)
+            
+            message = parse_status(homework)
+            send_message(bot, message)
+
+        time.sleep(RETRY_PERIOD)
 
 
 if __name__ == '__main__':
